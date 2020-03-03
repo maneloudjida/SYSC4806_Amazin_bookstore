9ca15dba5d8a173625bc83be6cc714c974b403ef
rayhaandustagheer <rayhaandtg@yahoo.com>
2020-03-03 15:42:17 -0500

    Added all the test classes for the entities

---
+++ src/test/java/entity/UserTest.java
@@ -0,0 +1,53 @@
+package entity;
+
+import  org.assertj.core.api.Assert;
+
+import static org.junit.jupiter.api.Assertions.*;
+public class UserTest {
+    User user;
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+        this.user= new User(1L, "1010", "Kyle", "Smith", "kyle@yahoo.com", "hello", "hel", Role.ROLE_CUSTOMER);
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getFname(){
+        assertEquals(user.getFname(),"Kyle");
+    }
+    @org.junit.jupiter.api.Test
+    void setFname(){
+        user.setFname("adi");
+        assertEquals(user.getFname(),"adi");
+    }
+    @org.junit.jupiter.api.Test
+    void getLname(){
+        assertEquals(user.getLname(),"Smith");
+    }
+    @org.junit.jupiter.api.Test
+    void setLname(){
+        user.setLname("Meet");
+        assertEquals(user.getLname(),"Meet");
+    }
+    @org.junit.jupiter.api.Test
+    void getPassword(){
+        assertEquals(user.getPassword(),"Hello");
+    }
+    @org.junit.jupiter.api.Test
+    void setPassword(){
+        user.setPassword("Meet");
+        assertEquals(user.getLname(),"Meet");
+    }
+    @org.junit.jupiter.api.Test
+    void getRole(){
+        assertEquals(user.getPassword(),Role.ROLE_CUSTOMER);
+    }
+    @org.junit.jupiter.api.Test
+    void setRole(){
+        user.setRole(Role.ROLE_BOOKSTOREOWNER);
+        assertEquals(user.getPassword(),Role.ROLE_BOOKSTOREOWNER);
+    }
+}
